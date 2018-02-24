c98b239b715e65bd44fad03d233e99a7e94f98fb
Rafael <rafael.machado@nodo.cc>
2018-02-23 22:32:59 -0300

    Add MainAdapter

--- app/src/main/java/com/example/rafael/chat/ui/MainAdapter.kt
+++ app/src/main/java/com/example/rafael/chat/ui/MainAdapter.kt
@@ -1,18 +1,19 @@
 package com.example.rafael.chat.ui
 
 import android.support.v7.widget.RecyclerView
-import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
-
+import android.view.ViewGroup
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.example.rafael.chat.R
-
 
 
 /**
  * Created by Rafael on 22/02/2018.
  */
-class MainAdapter: RecyclerView.Adapter(MainAdapter.ViewHolder) {
+class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -21,15 +22,21 @@ class MainAdapter: RecyclerView.Adapter(MainAdapter.ViewHolder) {
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return
-    }
+    override fun getItemCount(): Int { return 0 }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        return
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) { }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        @BindView(R.id.text_user1)
+        lateinit var text1: TextView
+
+        @BindView(R.id.text_user2)
+        lateinit var text3: TextView
+
+        init {
+            ButterKnife.bind(this, itemView)
+        }
+
     }
 
 }
