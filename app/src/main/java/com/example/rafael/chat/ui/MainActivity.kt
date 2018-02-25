afd4339a72196d52412eae34fe136c36854c4f71
Rafael <rafael.machado@nodo.cc>
2018-02-24 23:03:34 -0300

    Add connection with firebase

--- app/src/main/java/com/example/rafael/chat/ui/MainActivity.kt
+++ app/src/main/java/com/example/rafael/chat/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.rafael.chat.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import butterknife.BindView
 
 import butterknife.ButterKnife
 import butterknife.OnClick
@@ -10,6 +12,9 @@ import com.example.rafael.chat.R
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.edit_text)
+    lateinit var input: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +25,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     @OnClick(R.id.button_send)
-    fun sendMessage(input: String) {
+    fun sendMessage() {
+        if(input.toString() != "")
         FirebaseDatabase.getInstance()
                 .reference
                 .push()
-                .setValue(input)
+                .setValue(input.toString())
 
     }
 
