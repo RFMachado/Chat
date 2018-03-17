5763ff25a9d97404b2534eab73ad491ad47e7150
Rafael <rafael.machado@nodo.cc>
2018-03-16 23:02:42 -0300

    EventListener add.

---
+++ app/src/main/java/com/example/rafael/chat/Infrastructure/MessageMapper.kt
@@ -0,0 +1,8 @@
+package com.example.rafael.chat.Infrastructure
+
+/**
+ * Created by Rafael on 16/03/2018.
+ */
+class MessageMapper {
+
+}
--- app/src/main/java/com/example/rafael/chat/ui/MainActivity.kt
+++ app/src/main/java/com/example/rafael/chat/ui/MainActivity.kt
@@ -10,17 +10,52 @@ import butterknife.OnClick
 
 import com.example.rafael.chat.R
 import com.example.rafael.chat.domain.Message
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import com.google.firebase.database.DatabaseReference
+
 
 class MainActivity : AppCompatActivity() {
 
     @BindView(R.id.edit_text)
     lateinit var input: EditText
 
+    private val messageList: ArrayList<Message>? = null
+
+    lateinit var childEventListener: ChildEventListener
+    lateinit var mMessageReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
+
+        mMessageReference = FirebaseDatabase.getInstance().getReference("message")
+
+        childEventListener = object : ChildEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+            }
+
+            override fun onChildMoved(p0: DataSnapshot?, p1: String?) {
+            }
+
+            override fun onChildChanged(p0: DataSnapshot?, p1: String?) {
+                println("<<< Changed")
+            }
+
+            override fun onChildAdded(dataSnapshot: DataSnapshot?, p1: String?) {
+                println("<<< Add")
+
+               // val message = dataSnapshot?.getValue(Message::class.java)
+              //  messageList?.add(message!!)
+
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot?) {
+            }
+
+        }
+
+        mMessageReference.addChildEventListener(childEventListener)
 
        // val recyclerView = note_list_recyclerview
      //   recyclerView.adapter = MainAdapter(messageReceived(), this)
@@ -38,14 +73,5 @@ class MainActivity : AppCompatActivity() {
 
         input.text.clear()
     }
-
-//    fun messageReceived(): List<Message> {
-//
-//       val listMessage = FirebaseDatabase.getInstance()
-//                .getReference("message")
-//                .database
-//
-//        return listMessage
-//    }
 
 }
